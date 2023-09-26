a164cac24f5e246b70b2920519521be46573fb58
33kumarram <33kumarram@gmail.com>
2023-09-26 23:09:44 +0530

    created update price functionality

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,11 +1,13 @@
 const asyncHandler = require('express-async-handler')
 const Products = require("../models/productModel")
+const products = require('../models/productModel')
 
 
 const fetchProductsByCategory= asyncHandler(async(req, res)=>{
     try{
-        const category = req.params.category
-     const products = await Products.find({category})
+     const category = req.params.category
+        // console.log('category',category)
+     const products = await Products.find({category:category})
      res.status(201).json(products)
     }catch(err){
         console.log(err)
@@ -30,6 +32,9 @@ const fetchCategories= asyncHandler(async(req, res)=>{
             count: 1,
           },
         },
+        {
+          $sort:{category:1}
+        }
       ]);
 
       res.status(201).json(categories)
@@ -41,7 +46,22 @@ const fetchCategories= asyncHandler(async(req, res)=>{
     }
 })
 
+const updatePrice =asyncHandler(async(req,res)=>{
+try{
+ const data = req.body
+//  console.log(data)
+ data.map(async(product)=>{
+  await products.findByIdAndUpdate(product._id,{price:product.price})
+ })
+ res.status(201).json({message:"price updated"})
+}catch(err){
+  console.log(err)
+  res.status(400)
+  throw new Error('Error occurred while updating price')
+}
+})
 module.exports = {
     fetchProductsByCategory,
-    fetchCategories
+    fetchCategories,
+    updatePrice
 }
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,11 +1,12 @@
 const express = require("express")
 
-const {fetchProductsByCategory, fetchCategories} = require("../controllers/productController")
+const {fetchProductsByCategory, fetchCategories, updatePrice} = require("../controllers/productController")
 
 const router = express.Router()
 
 router.route('/productsbycategory/:category').get(fetchProductsByCategory)
 router.route('/categories').get(fetchCategories)
+router.route('/updateprice').put(updatePrice)
 
 module.exports= router
 
